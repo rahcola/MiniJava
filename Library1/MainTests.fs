74ff4b1f7446b3d680ba4c3626a85b37bb5bfcca
Jani Rahkola <jani.rahkola@iki.fi>
2014-03-14 19:49:28 +0000

    init

---
+++ Library1/MainTests.fs
@@ -0,0 +1,8 @@
+﻿module MainTests
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``addition`` () =
+    Main.foo 1 |> should equal 2
---
+++ MiniJava/Main.fs
@@ -0,0 +1,8 @@
+﻿module Main
+
+let foo x = x + 1
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
